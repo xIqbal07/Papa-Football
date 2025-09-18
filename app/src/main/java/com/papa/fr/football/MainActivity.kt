38b3c8a7a93fa43653b0ab4da80aad8ca6b8b131
xIqbal07 <147745976+xIqbal07@users.noreply.github.com>
2025-09-18 21:20:18 +0700

    Fix matches tab selection wiring

--- app/src/main/java/com/papa/fr/football/MainActivity.kt
+++ app/src/main/java/com/papa/fr/football/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.papa.fr.football.common.matches.MatchesTabLayoutView
 import com.papa.fr.football.databinding.ActivityMainBinding
 import com.papa.fr.football.matches.MatchesListFragment
+import com.papa.fr.football.matches.MatchesTabType
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,23 +21,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(binding.bottomNavigation) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(v.paddingLeft, v.paddingTop, v.paddingRight, systemBars.bottom)
             insets
         }
 
+        setupMatchesTabs()
+        setupBottomNav(binding.bottomNavigation)
+    }
+
+    private fun setupMatchesTabs() {
         binding.matchesTabs.setupWith(
             fragmentActivity = this,
             tabs = listOf(
                 MatchesTabLayoutView.TabItem(getString(R.string.matches_tab_future)) {
-                    MatchesListFragment.newInstance(getString(R.string.matches_tab_future))
+                    MatchesListFragment.newInstance(MatchesTabType.FUTURE)
                 },
                 MatchesTabLayoutView.TabItem(getString(R.string.matches_tab_live)) {
-                    MatchesListFragment.newInstance(getString(R.string.matches_tab_live))
+                    MatchesListFragment.newInstance(MatchesTabType.LIVE)
                 },
                 MatchesTabLayoutView.TabItem(getString(R.string.matches_tab_past)) {
-                    MatchesListFragment.newInstance(getString(R.string.matches_tab_past))
+                    MatchesListFragment.newInstance(MatchesTabType.PAST)
                 }
             )
         )
+    }
+
+    private fun setupBottomNav(bottomNav: BottomNavigationView) {
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_schedule -> true
+                else -> false
+            }
+        }
+        bottomNav.selectedItemId = R.id.menu_schedule
     }
 }
